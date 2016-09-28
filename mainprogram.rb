55fad4eb6ac51974d127d6819f03290ad10f307c
Lillie Madali <lillie.madali@gmail.com>
2016-09-28 18:55:27 -0400

    first commit

---
+++ mainprogram.rb
@@ -0,0 +1,12 @@
+system "clear"
+system ('say "You\'ve got mail"'')
+puts "*************************************************"
+puts "****  Welcome to the Car Rental Application  ****"
+puts "*************************************************"
+puts "****  Please select from the following menu: ****"
+puts "**** 1. Option 1"
+puts "**** 2. Option 2"
+puts "**** 3. Option 3"
+puts "**** 4. Option 4"
+option = gets.chomp
+puts "You chose option #{option}."
