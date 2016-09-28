2c600adb506647f4ce507fa02a1e4c645b74e712
Lillie Madali <lillie.madali@gmail.com>
2016-09-28 19:22:20 -0400

    Added menu method

--- mainprogram.rb
+++ mainprogram.rb
@@ -1,24 +1,57 @@
-system "clear"
-puts "*************************************************"
-puts "****  Welcome to the Car Rental Application  ****"
-puts "*************************************************"
-puts "****  Please select from the following menu: ****"
-puts "**** 1. Option 1"
-puts "**** 2. Option 2"
-puts "**** 3. Option 3"
-puts "**** 4. Option 4"
+def display_menu
+	system "clear"
+	puts "*************************************************"
+	puts "****  Welcome to the Car Rental Application  ****"
+	puts "*************************************************"
+	puts "****  Please select from the following menu: ****"
+	puts "**** 1. Option 1"
+	puts "**** 2. Option 2"
+	puts "**** 3. Option 3"
+	puts "**** 4. Option 4"
+	
+	option = gets.chomp.to_i
 
-option = gets.chomp.to_i
+	case option
+	when 1
+		puts "1"
+	when 2
+		puts "2"
+	when 3
+		puts "3"
+	when 4
+		puts "4"
+	else
+		puts "Wrong option."
+	end
 
-case option
-when 1
-	puts "1"
-when 2
-	puts "2"
-when 3
-	puts "3"
-when 4
-	puts "4"
-else
-	puts "Wrong option."
 end
+
+display_menu
+
+class Vehicle
+	def initialize(year,make,model)
+		@year = year
+		@make = make
+		@model = model
+	end
+
+	def year
+		@year
+	end
+
+	def make
+		@make
+	end
+
+	def model
+		@model
+	end
+
+	def greeting
+		puts "pssssstttt!!"
+	end
+end
+
+Accord = Vehicle.new("2016","Honda","Accord")
+
+puts "Hi, I have an #{Accord.model} and its year is #{Accord.year}."
