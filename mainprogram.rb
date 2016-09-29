90218d2c5ad0f03382594b4640e58ad1a661654d
Lillie Madali <lillie.madali@gmail.com>
2016-09-28 20:33:24 -0400

    Added more menu items

--- mainprogram.rb
+++ mainprogram.rb
@@ -1,3 +1,6 @@
+@parking_lot = []
+@message = ""
+
 class Vehicle
 	def initialize(year,make,model)
 		@year = year
@@ -16,41 +19,77 @@ class Vehicle
 	def model
 		@model
 	end
-
 end
 
-def display_menu
-	system "clear"
+def display_menu clear_menu
+	if clear_menu == "YES"
+		system "clear"
+	end
 	puts "*************************************************"
 	puts "****  Welcome to the Car Rental Application  ****"
 	puts "*************************************************"
 	puts "****  Please select from the following menu: ****"
-	puts "**** 1. Option 1"
-	puts "**** 2. Option 2"
+	puts "**** 1. Add a Vehicle"
+	puts "**** 2. Print Vehicles"
 	puts "**** 3. Option 3"
 	puts "**** 4. Option 4"
+	puts "**** 5. Exit Application"
 
+	status_message(@message)
+	@message = ""
 	option = gets.chomp.to_i
 
 	case option
 	when 1
-		puts "1"
+		add_a_vehicle
 	when 2
-		puts "2"
+		print_vehicles
 	when 3
 		puts "3"
 	when 4
 		puts "4"
+	when 5
+		exit_application
 	else
 		puts "Wrong option."
 	end
-
 end
 
-# display_menu
+def add_a_vehicle
+	puts "What year was your vehicle made?"
+	year = gets.chomp
+	puts "What make of vehicle do you have?"
+	make = gets.chomp
+	puts "What model is your vehicle?"
+	model = gets.chomp
 
-my_awesome_car = Vehicle.new(2016,"Honda","Accord")
+	vehicle = Vehicle.new(year,make,model)
+	
+	@parking_lot.push(vehicle)
 
-puts "Hi, I have an #{my_awesome_car.model} and its year is #{my_awesome_car.year}."
+	@message = "Added a vehicle"
 
-puts my_awesome_car.year
+	display_menu("YES")
+end
+
+def exit_application
+	puts "*************************************************"
+	puts "******************Bye Bye************************"
+	puts "*************************************************"
+end
+
+def status_message message
+	puts "*************************************************"
+	puts "********************* #{message} **********************"
+	puts "*************************************************"
+end 
+
+def print_vehicles
+	puts "The vehicles on your lot are:"
+	@parking_lot.each do |cars|
+		puts "#{car.year} #{car.make} #{car.model}"
+	end
+	display_menu("NO")
+end
+
+display_menu("YES")
